e92879a55328262e772ec75e77108bede23bd2e6
ali atwa <ali.atwa2040@gmail.com>
2024-05-22 07:16:31 +0300

    note

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import Button from "../../components/UI/Button";
 import { useGlobalContext } from "../../store/chromeStore";
 import { useToast } from "../../components/Toast/ToastService";
+import { constants } from "../../utils/constants";
 
 const Home: React.FC = () => {
   const { token, loading, setLoading } = useGlobalContext();
@@ -23,10 +24,18 @@ const Home: React.FC = () => {
   return (
     <>
       <div className="flex flex-col gap-4 justify-between items-center bg-white rounded-t-xl w-full h-full">
-        <div className="flex flex-row justify-center items-center w-full h-full">
+        <div className="flex flex-col gap-6 justify-center items-center w-full h-full">
           <Button disabled={loading} onClick={() => handleFillBtn()}>
             {loading ? "Loading" : "Fill"}
           </Button>
+
+          <div className="w-[180px] rounded-xl  border border-solid border-[#0b2938] p-2 text-xs text-[#0b2938]">
+            Ensure to fill all your{" "}
+            <a href={constants.base_url} target="_blank">
+              profile
+            </a>{" "}
+            details in the web app to get best results
+          </div>
         </div>
       </div>
     </>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const constants = {
+  base_url: "http://localhost:3000",
   login_url: "/login",
   register_url: "/register",
   base_api_url: "http://localhost:8000/api/",
